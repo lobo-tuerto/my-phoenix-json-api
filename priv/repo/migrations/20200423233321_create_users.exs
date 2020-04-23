5c37ceb5ea6f062364bda833946d38bbb3fa2c74
Víctor Adrián <adriandcs@gmail.com>
2020-04-23 18:36:15 -0500

    Add User schema

---
+++ priv/repo/migrations/20200423233321_create_users.exs
@@ -0,0 +1,16 @@
+defmodule MyApp.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :email, :string, null: false
+      add :password_hash, :string
+      add :is_active, :boolean, default: false, null: false
+
+      timestamps()
+    end
+
+    create unique_index(:users, [:email])
+  end
+end
