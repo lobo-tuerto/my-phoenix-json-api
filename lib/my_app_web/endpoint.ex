cd596eebc1e19c3f8d9f398a889737cdc3f940f2
Víctor Adrián <adriandcs@gmail.com>
2020-04-23 21:00:33 -0500

    Configure CORS support

--- lib/my_app_web/endpoint.ex
+++ lib/my_app_web/endpoint.ex
@@ -48,5 +48,12 @@ defmodule MyAppWeb.Endpoint do
   plug Plug.MethodOverride
   plug Plug.Head
   plug Plug.Session, @session_options
+
+  plug Corsica,
+    origins: "http://localhost:8080",
+    allow_credentials: true,
+    allow_headers: ["Content-Type"],
+    log: [rejected: :error, invalid: :warn, accepted: :debug]
+
   plug MyAppWeb.Router
 end
--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule MyApp.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:bcrypt_elixir, "~> 2.0"}
+      {:bcrypt_elixir, "~> 2.0"},
+      {:corsica, "~> 1.0"}
     ]
   end
 
