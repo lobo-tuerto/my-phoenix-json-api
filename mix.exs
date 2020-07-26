315ce0d8c032d54a49caf8e0ca4c98e7dfe3a30b
Víctor Adrián <adriandcs@gmail.com>
2020-07-25 22:43:41 -0500

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MyApp.MixProject do
   def project do
     [
       app: :my_app,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
