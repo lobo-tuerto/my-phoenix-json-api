302f3cff64843be31170c9eeffe941d05dda72b2
Víctor Adrián <adriandcs@gmail.com>
2020-07-25 20:52:57 -0500

    Add some user seed data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,8 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+IO.puts("Adding a couple of users...")
+
+MyApp.Auth.create_user(%{email: "user1@email.com", password: "qwerty"})
+MyApp.Auth.create_user(%{email: "user2@email.com", password: "asdfgh"})
